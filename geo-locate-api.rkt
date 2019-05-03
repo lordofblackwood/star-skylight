874fb036fab17a0ea33fe93c20e8142572df1a6d
michaelkhalil <khalil.m@husky.neu.edu>
2019-05-03 16:42:17 -0400

    Can now get coords from gmaps api

--- geo-locate-api.rkt
+++ geo-locate-api.rkt
@@ -1,6 +1,15 @@
-(require 
+#lang racket/base
+(require net/url
+         json
+         "config.rkt") 
 
-(define API )
+(provide coords)
 
 (define base-url "https://www.googleapis.com/geolocation/v1/geolocate?key=")
-(define geo-url (string->url (string-append base-url API)))
+(define geo-url (string->url (string-append base-url API-KEY)))
+
+(define coords
+  (let ([geo-info (read-json (post-pure-port geo-url #""))])
+    (list (hash-ref geo-info "latitude") 
+          (hash-ref geo-info "longitude"))))
+
