dfeaf39c36bf0edac8bdc96d742389a315f7a89d
michaelkhalil <khalil.m@husky.neu.edu>
2019-10-22 23:59:13 -0400

    Updating to output a coords object with current location

--- geo-locate-api.rkt
+++ geo-locate-api.rkt
@@ -3,13 +3,17 @@
          json
          "config.rkt") 
 
-(provide coords)
+(provide LOCATION 
+         (struct-out coords))
 
+(struct coords [latitude longitude] #:transparent)
 (define base-url "https://www.googleapis.com/geolocation/v1/geolocate?key=")
 (define geo-url (string->url (string-append base-url API-KEY)))
 
-(define coords
-  (let ([geo-info (read-json (post-pure-port geo-url #""))])
-    (list (hash-ref geo-info "latitude") 
-          (hash-ref geo-info "longitude"))))
+(define LOCATION
+  (let* ([geo-info   (read-json (post-pure-port geo-url #""))]
+         [location   (hash-ref geo-info 'location)]
+         [lat        (hash-ref location 'lat)]
+         [long       (hash-ref location 'lng)])
+    (coords lat long)))
 
