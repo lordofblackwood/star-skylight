2394ba0ee6f56baac1aafcd7b9d8303ca9f5c14e
michaelkhalil <khalil.m@husky.neu.edu>
2019-10-23 00:00:10 -0400

    Beginning to actually design the program after playing around with how some of these racket libraries work.

--- star-skylight.rkt
+++ star-skylight.rkt
@@ -1,10 +1,14 @@
 #lang racket/base
 (require "geo-locate-api.rkt"
-         )
+         "solar-data.rkt"
+         gregor
+         racket/date
+         racket/list
+         racket/pretty)
 
 
-(define latitude (first coords))
-(define longitude (first coords))
+(define latitude (coords-latitude LOCATION))
+(define longitude (coords-longitude LOCATION))
 
 ;; API requires right ascension and declination
 ;; For our purposes we will say the part of the galaxay
@@ -20,11 +24,28 @@
 ;; sun location/right ascension and for yesterday/today/tomorrow
 ;; and then go the approrpiate percentage between them.
 
-(define RIGHT-ASCENSION )
+;(define RIGHT-ASCENSION )
 
-(define init-local-noon )
-(define final-local-noon )
-(define aprox-time )
-(define sun-init-ra )
-(define sun-final-ra )
+(date-display-format 'iso-8601) 
+(define current-time (now/utc))
+(define curdate (date->string (current-date)))
+(define local-noon-datetime (string-append curdate " " (solar-noon latitude longitude "today")))
+(define today-local-noon (parse-datetime local-noon-datetime "yyyy-mm-dd h:mm:ss aa"))
+(pretty-print current-time)
+(pretty-print today-local-noon)
+
+;(define sun-init-ra )
+;(define sun-final-ra )
+
+;(-> Image)
+; Returns an image of the stars directly above you.
+#;(define (get-stars-above-me)
+  (let ([declincation (coords-longitude location)]
+        [right-ascention (calc-right-ascention)])
+    (get-nasa-image declintation right-asenction)))
+
+;(-> Real)
+; Calculates the right ascention for the space that is above you right now.
+;(define 
+
 
