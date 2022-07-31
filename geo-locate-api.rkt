5318517648f920f35f787540f13164c1b2317668
michaelkhalil <khalil.m@husky.neu.edu>
2022-07-31 11:56:17 -0400

    Refactoring for easier use with aws lambda

--- geo-locate-api.rkt
+++ geo-locate-api.rkt
@@ -3,14 +3,14 @@
          json
          "config.rkt") 
 
-(provide LOCATION 
+(provide get-current-location
          (struct-out coords))
 
 (struct coords [latitude longitude] #:transparent)
 (define base-url "https://www.googleapis.com/geolocation/v1/geolocate?key=")
 (define geo-url (string->url (string-append base-url API-KEY)))
 
-(define LOCATION
+(define (get-current-location)
   (let* ([geo-info   (read-json (post-pure-port geo-url #""))]
          [location   (hash-ref geo-info 'location)]
          [lat        (hash-ref location 'lat)]
--- star-skylight.rkt
+++ star-skylight.rkt
@@ -12,21 +12,21 @@
          racket/pretty)
 
 
-(define latitude (coords-latitude LOCATION))
-(define longitude (coords-longitude LOCATION))
-
 ;; API requires right ascension and declination
 ;; For our purposes we will say the part of the galaxay
 ;; that is right above you, is where you are vertically
 ;; on the earth and how far the earth has turned from the
 ;; the sun.
 ;
+;; That is to say declination = latitude
+;; and right ascension = (Sun's RA@next solar noon - Sun's RA@prev solar noon) * % of time passed between the two
+;
 
 ;; Right ascension, we will look at when local noon was
 ;; yesterday and today or today and tomorrow and see what
-;; percent we are inbetween and then look at the corresponding
+;; percent we are in-between and then look at the corresponding
 ;; sun location/right ascension and for yesterday/today/tomorrow
-;; and then go the approrpiate percentage between them.
+;; and then go the appropriate percentage between them.
 
 
 (date-display-format 'iso-8601) 
@@ -35,24 +35,22 @@
 ;(define local-noon-datetime (string-append curdate " " (solar-noon latitude longitude "today")))
 ;(define today-local-noon (parse-datetime local-noon-datetime "yyyy-MM-dd h:mm:ss aa"))
 
-;(-> Real Real Bitmap)
+;(-> Real Real Datetime Datetime Bitmap)
 ; Returns an image of the stars directly above you.
-(define (get-stars-above-me lng lat)
-  (let ([declination (number->string lng)]
-        [right-ascension (number->string (calc-right-ascension lng lat))])
+(define (get-stars-above-me lng lat now today)
+  (let ([declination (number->string lat)]
+        [right-ascension (number->string (calc-right-ascension lng lat now today))])
     (get-nasa-image declination right-ascension)))
 
-;(-> Real Real Real)
+;(-> Real Real Dateime Datetime Datetime Real)
 ; Calculates the right ascention for the space that is above you right now.
-(define (calc-right-ascension lng lat)
+(define (calc-right-ascension lng lat now todays-date)
   ;(display (map string->number (string-split (date->iso8601 (today/utc)) "-")))
-    (let* ([now                       (now/utc)]
-           [todays-date               (today/utc)]
-           [today                     (date->iso8601 todays-date)]
+    (let* ([today                     (date->iso8601 todays-date)]
            [yesterday                 (date->iso8601 (-days todays-date 1))]
            [tomorrow                  (date->iso8601 (+days todays-date 1))]
-           [solar-noon-pair           (get-solar-noon-pair now yesterday today tomorrow latitude longitude)]
-           [ra-pair                   (get-ra-pair now yesterday today tomorrow latitude longitude )]
+           [solar-noon-pair           (get-solar-noon-pair now yesterday today tomorrow lat lng)]
+           [ra-pair                   (get-ra-pair now yesterday today tomorrow lat lng)]
            [percentage-between-noons  (percent-of-day-completed now solar-noon-pair)]
            [degrees-to-travel         (+ 360 (- (second ra-pair) (first ra-pair)))])
       (+ (car ra-pair) (* percentage-between-noons degrees-to-travel))))
@@ -89,13 +87,20 @@
 ; Returns what percentage of the day has been completed,
 ; where the day is defined as the time between two solar noons
 (define (percent-of-day-completed now solar-noon-pair)
-  (let* ([as-datetime       (lambda (time)
+  (let* ([as-datetime   (lambda (time)
                               (parse-datetime time "yyyy-MM-dd h:mm:ss aa"))]
         [solar-day      (minutes-between (as-datetime (first solar-noon-pair))
                                          (as-datetime (second solar-noon-pair)))]
         [day-completed  (minutes-between (as-datetime (first solar-noon-pair)) now)])
     (/ (->fl day-completed) solar-day)))
 
-(define f (new frame% [label "Stars Above me Right Now"]))
-(send f show #t)
-(void (new message% [parent f] [label (get-stars-above-me longitude latitude)]))
+(module+ main 
+  (define f (new frame% [label "Stars Above me Right Now"]))
+  (define LOCATION (get-current-location))
+  (send f show #t)
+  (void (new message%
+             [parent f]
+             [label (get-stars-above-me (coords-longitude LOCATION)
+                                        (coords-latitude LOCATION)
+                                        (now/utc)
+                                        (today/utc))])))
